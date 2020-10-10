9727e2f3e907b5c768ac33c61f4ed6d73a753c74
Ethan Whittemore <erw444@gmail.com>
2020-10-10 18:09:43 -0500

    Rename java files and nav graph for fragments.

--- app/src/main/java/com/erw/randomrestaurant/AddListFragment.java
+++ app/src/main/java/com/erw/randomrestaurant/AddListFragment.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class SecondFragment extends Fragment {
+public class AddListFragment extends Fragment {
 
     @Override
     public View onCreateView(
@@ -17,17 +17,17 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return inflater.inflate(R.layout.fragment_add_list, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                NavHostFragment.findNavController(AddListFragment.this)
+                        .navigate(R.id.action_AddListFragment_to_AddRestaurantFragment);
             }
         });
     }
